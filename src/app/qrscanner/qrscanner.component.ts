a8c634cf6f124948a768c1c52a6ed5ee81a26200
unknown <malinduruwantha@gmail.com>
2019-03-13 21:47:09 +0530

    added qr ccode

---
+++ src/app/qrscanner/qrscanner.component.ts
@@ -0,0 +1,44 @@
+
+import {Component, ViewChild, ViewEncapsulation, OnInit} from '@angular/core';
+import {QrScannerComponent} from 'angular2-qrscanner';
+@Component({
+  selector: 'app-qrscanner',
+  templateUrl: './qrscanner.component.html',
+  styleUrls: ['./qrscanner.component.css']
+})
+export class QrscannerComponent implements OnInit {
+  @ViewChild(QrScannerComponent) qrScannerComponent: QrScannerComponent ;
+  constructor() { }
+
+  ngOnInit() {
+    this.qrScannerComponent.getMediaDevices().then(devices => {
+      console.log(devices);
+      const videoDevices: MediaDeviceInfo[] = [];
+      for (const device of devices) {
+          if (device.kind.toString() === 'videoinput') {
+              videoDevices.push(device);
+          }
+      }
+      if (videoDevices.length > 0){
+          let choosenDev;
+          for (const dev of videoDevices){
+              if (dev.label.includes('front')){
+                  choosenDev = dev;
+                  break;
+              }
+          }
+          if (choosenDev) {
+              this.qrScannerComponent.chooseCamera.next(choosenDev);
+          } else {
+              this.qrScannerComponent.chooseCamera.next(videoDevices[0]);
+          }
+      }
+  });
+
+  this.qrScannerComponent.capturedQr.subscribe(result => {
+      console.log(result);
+  });
+}
+  }
+
+
