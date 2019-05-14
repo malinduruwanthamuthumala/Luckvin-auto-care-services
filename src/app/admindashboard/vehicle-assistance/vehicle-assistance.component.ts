01250421ab30631ce20698e6830a5f34a1bf4f8b
unknown <malinduruwantha@gmail.com>
2019-05-14 14:21:46 +0530

    changed vehicle assistant component

--- src/app/admindashboard/vehicle-assistance/vehicle-assistance.component.ts
+++ src/app/admindashboard/vehicle-assistance/vehicle-assistance.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/shared/services/auth.service';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Breakdown } from 'src/app/shared/breakdown.model';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-vehicle-assistance',
@@ -12,10 +19,25 @@ export class VehicleAssistanceComponent implements OnInit {
 
   public origin: any;
   public destination: any;
-  constructor() { }
+  list=[];
+  status='';
+  breakdownef:AngularFirestoreCollection<Breakdown>;
+  breakdown$:Observable<Breakdown[]>;
+  constructor(
+    public authService: AuthService,
+    public afs: AngularFirestore,   // Inject Firestore service
+    public afAuth: AngularFireAuth,
+    private router: Router,
+    private af: AuthService,
+    private firestore: AngularFirestore,
+    private toastr: ToastrService,
+  ) { 
+   
+  }
 
   ngOnInit() {
     this.getDirection();
+    this.getall();
   }
 
 
@@ -27,4 +49,26 @@ export class VehicleAssistanceComponent implements OnInit {
     // this.origin = 'Taipei Main Station';
     // this.destination = 'Taiwan Presidential Office';
   }
+  getall(){
+    this.afs.collection('Breakdowns').snapshotChanges().subscribe(actionArray => {
+      this.list = actionArray.map(item=>{
+         return { 
+           id: item.payload.doc.id,
+           ...item.payload.doc.data()
+         } as Breakdown
+     
+       })
+      
+     });
+    
+  }
+  showlocation(lat:number,long:number,id:string){
+    this.lat=lat;
+    this.lng=long;
+    let data = Object.assign({});
+    data.status="seen";
+    this.firestore.collection('Breakdowns').doc(id).update(data);
+   
+  }
+  
 }
